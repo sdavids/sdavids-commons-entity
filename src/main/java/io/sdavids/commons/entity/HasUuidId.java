e620ec50410520191397c21100e2a59c7c8d6330
Sebastian Davids <sdavids@gmx.de>
2017-05-31 22:23:26 +0200

    Add HasUuidId
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ src/main/java/io/sdavids/commons/entity/HasUuidId.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) 2017, Sebastian Davids
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sdavids.commons.entity;
+
+import java.util.UUID;
+
+/**
+ * A marker interface for an object having a {@code UUID} ID.
+ *
+ * @see UUID
+ * @since 1.0
+ */
+@FunctionalInterface
+public interface HasUuidId {
+
+  /**
+   * Returns the {@code UUID} ID from the given object.
+   *
+   * @param obj the object; null returns null
+   * @return the ID; may be null
+   * @since 1.0
+   */
+  static UUID uuidIdFrom(HasUuidId obj) {
+    return obj == null ? null : obj.getId();
+  }
+
+  /**
+   * Returns whether the given object has an ID.
+   *
+   * @param obj the object; null returns false
+   * @return true if it has an ID; false otherwise
+   * @see java.util.function.Predicate
+   * @since 1.0
+   */
+  static boolean nonNullUuidId(HasUuidId obj) {
+    return !(obj == null || obj.getId() == null);
+  }
+
+  /**
+   * Returns either the passed in object, or if the {@code HasUuidId} is null or its ID is null,
+   * null.
+   *
+   * @param obj the {@code HasUuidId} to check; may be null
+   * @return the passed {@code HasUuidId}, or null if it was null or its ID was null
+   * @since 1.0
+   */
+  static HasUuidId nullIfUuidIdNull(HasUuidId obj) {
+    return obj == null ? null : obj.getId() == null ? null : obj;
+  }
+
+  /**
+   * Returns the ID of this object.
+   *
+   * @return the ID; may be null
+   * @since 1.0
+   */
+  UUID getId();
+
+  /**
+   * Returns whether this object has an ID.
+   *
+   * @return true if it has an ID; false otherwise
+   * @since 1.0
+   */
+  default boolean hasId() {
+    return getId() != null;
+  }
+}
---
+++ src/test/java/io/sdavids/commons/entity/HasUuidIdTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2017, Sebastian Davids
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sdavids.commons.entity;
+
+import static io.sdavids.commons.entity.HasUuidId.nonNullUuidId;
+import static io.sdavids.commons.entity.HasUuidId.nullIfUuidIdNull;
+import static io.sdavids.commons.entity.HasUuidId.uuidIdFrom;
+import static java.util.UUID.fromString;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+import static org.mockito.quality.Strictness.STRICT_STUBS;
+
+import java.util.UUID;
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+public final class HasUuidIdTest {
+
+  private static final UUID UUID = fromString("1833e288-f477-48f2-81da-3ce6fde82253");
+
+  @Rule public MockitoRule rule = MockitoJUnit.rule().strictness(STRICT_STUBS);
+
+  @Mock private HasUuidId hasUuidId;
+
+  @Test
+  public void hasId_withId_null() {
+    when(hasUuidId.getId()).thenReturn(null);
+    when(hasUuidId.hasId()).thenCallRealMethod();
+
+    assertThat(hasUuidId.hasId()).isFalse();
+  }
+
+  @Test
+  public void hasId_withId() {
+    when(hasUuidId.getId()).thenReturn(UUID);
+    when(hasUuidId.hasId()).thenCallRealMethod();
+
+    assertThat(hasUuidId.hasId()).isTrue();
+  }
+
+  @Test
+  public void nonNullUuidId_null() {
+    assertThat(nonNullUuidId(null)).isFalse();
+  }
+
+  @Test
+  public void nonNullUuidId_withId_null() {
+    when(hasUuidId.getId()).thenReturn(null);
+
+    assertThat(nonNullUuidId(hasUuidId)).isFalse();
+  }
+
+  @Test
+  public void nonNullUuidId_withId() {
+    when(hasUuidId.getId()).thenReturn(UUID);
+
+    assertThat(nonNullUuidId(hasUuidId)).isTrue();
+  }
+
+  @Test
+  public void uuidIdFrom_null() {
+    assertThat(uuidIdFrom(null)).isNull();
+  }
+
+  @Test
+  public void uuidIdFrom_hasUuidId_returns_null() {
+    when(hasUuidId.getId()).thenReturn(null);
+
+    assertThat(uuidIdFrom(hasUuidId)).isNull();
+  }
+
+  @Test
+  public void uuidIdFrom_() {
+    when(hasUuidId.getId()).thenReturn(UUID);
+
+    assertThat(uuidIdFrom(hasUuidId)).isEqualTo(UUID);
+  }
+
+  @Test
+  public void nullIfUuidIdNull_null() {
+    assertThat(nullIfUuidIdNull(null)).isNull();
+  }
+
+  @Test
+  public void nullIfUuidIdNull_hasUuidId_returns_null() {
+    when(hasUuidId.getId()).thenReturn(null);
+
+    assertThat(nullIfUuidIdNull(hasUuidId)).isNull();
+  }
+
+  @Test
+  public void nullIfUuidIdNull_() {
+    when(hasUuidId.getId()).thenReturn(UUID);
+
+    assertThat(nullIfUuidIdNull(hasUuidId)).isEqualTo(hasUuidId);
+  }
+}
